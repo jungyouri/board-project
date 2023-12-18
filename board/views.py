a9067323e30c569d185fa2e187fb25e2bcf9b2dc
jungyouri <144638276+jungyouri@users.noreply.github.com>
2023-12-18 10:58:22 +0900

    Add files via upload

---
+++ board/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+#from django.http import HttpResponse
+
+# Create your views here.
+
+#def index(request):
+#    return HttpResponse("Board index page.")
+
+from .models import Board
+from django.views import generic
+from django.urls import reverse_lazy
+
+def index(request):
+    board_list = Board.objects.order_by('-create_date')
+    context = {'board_list': board_list}
+    return render(request, 'board/board_list.html', context)
+
+def detail(request, board_id):
+ board = Board.objects.get(id=board_id)
+ context = {'board': board}
+ return render(request, 'board/board_detail.html', context) 
+
+class create(generic.CreateView):
+ model = Board
+ fields = ['subject', 'content', 'create_date']
+ success_url = reverse_lazy('board:list')
+ template_name_suffix = '_create'
